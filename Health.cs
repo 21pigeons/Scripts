8cb5dbef05c121be8eb3d87b49c60325b8619f71
21pigeons <radiodreaming@gmail.com>
2016-08-07 22:14:51 +1200

    Initial Commit
    Initial Commit of scripts for controling player and camera-some
    collected and modifyed from the internet

---
+++ Health.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Health : MonoBehaviour {
+	
+	public Slider healthBarSlider;  //reference for slider
+	
+	//Check if player enters/stays on the fire
+	void OnTriggerStay(Collider other){
+		//if player triggers fire object and health is greater than 0
+		if(other.gameObject.name=="Health" && healthBarSlider.value>0){
+			healthBarSlider.value -=.011f;  //reduce health
+		}
+	}
+}
