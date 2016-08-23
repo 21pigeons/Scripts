0a4fa0706ffeaf5b2a821920914c2936d9ba094c
Jack Bannister-Sutton <jackbannistersutton@gmail.com>
2016-08-23 21:32:05 +1200

    Added Basic Canvas code

---
+++ Canvas.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Canvas : MonoBehaviour {
+    public bool isOpen = false;
+    public GameObject CraftingUI;
+
+        // Use this for initialization
+    void Start()
+    {
+
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isOpen = !isOpen;
+            CraftingUI.SetActive(isOpen);
+        }
+    }
+}
+
+
